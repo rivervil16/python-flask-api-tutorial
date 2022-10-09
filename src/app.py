df8e69f6c924340a29ed93e6f4a2f2e323461008
rivervil16 <agustinriverovila@gmail.com>
2022-10-09 04:40:57 +0000

    05 returning JSON

---
+++ src/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+todos = [ { "label": "My first task", "done": False } ]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = flask.jsonify(todos)
+    return json_text
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
